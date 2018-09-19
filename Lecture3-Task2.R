a2e3b57ba360a71aeb748cd0fbc8ec2c8dfea574
ashakhzodov <43409086+ashakhzodov@users.noreply.github.com>
2018-09-19 09:28:24 -0300

    Update Lecture3-Task2.R

--- Lecture3-Task2.R
+++ Lecture3-Task2.R
@@ -1,6 +1,6 @@
 #This is a random function to create a new data. 
 
-invlogit = function(x){exp(x)/(exp(x)+1)}
-temps = Temperature-mean(Temperature)
-xnew = invlogit(temps)
-plot(temps, xnew)
+xdata = rnorm(100)
+hist(xdata)
+
+
